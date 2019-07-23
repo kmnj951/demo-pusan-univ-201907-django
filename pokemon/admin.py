2fd01f47fd48daba73e0a5e78e7820b1378ce9f0
askcompany-kr <me@askcompany.kr>
2019-07-23 14:40:40 +0900

    Pokemon admin 구현

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Pokemon
+
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'page_tag']
+
+    def page_tag(self, pokemon):
+        tag = '<a href="{}" target="_blank">링크</a>'.format(pokemon.page_url)
+        return mark_safe(tag)
+
+
+admin.site.register(Pokemon, PokemonAdmin)
--- pokemon/models.py
+++ pokemon/models.py
@@ -5,3 +5,6 @@ class Pokemon(models.Model):
     name = models.CharField(max_length=100)
     page_url = models.URLField()
     desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
