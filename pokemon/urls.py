c2ecc15f404d8de5c4d3c50690a1e0d7c7d185a8
snowcrab <48473962+kmnj951@users.noreply.github.com>
2019-07-24 13:06:06 +0900

    html/css변경

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from pokemon.views import index, pokemon_new, pokemon_edit, pokemon_list_html
+from pokemon.views import index, pokemon_new, pokemon_edit
 
 urlpatterns = [
     path('', index),
-    path('pokemon_list.html', pokemon_list_html),
+    # path('pokemon_list.html', pokemon_list_html),
     path('new/', pokemon_new),  # django는 post 구분(new/post/) 안함
     path('<int:pk>/', pokemon_edit),    # pk도 넣어서 호출
     # re_path(r'(?P<pk>\d+)', pokemon_edit),
--- pokemon/views.py
+++ pokemon/views.py
@@ -3,17 +3,23 @@ from django.shortcuts import render
 
 from pokemon.models import Pokemon
 
+# urlpatterns에 추가해야 호출됨
+# 앱 폴더 내에 urls.py 만들어서 urlpattens에 추가 - 재사용성 (추가하기 편리)
+# 생성된 파일 최상위 urls.py 와 연결
+
 
 def index(request):
+    qs = Pokemon.objects.all()  # QuerySet Type (가져올 예정)
     # return render(request, 'root.html')
-    return render(request, 'pokemon/pokemon_list.html')
-
-
-def pokemon_list_html(request):
-    qs = Pokemon.objects.all()  # QuerySet 타입
-    return render(request, 'pokemon/pokemon_list_html.html', {
+    return render(request, 'pokemon/pokemon_list.html', {
         'pokemon_list': qs,
     })
+
+# def pokemon_list_html(request):
+#     qs = Pokemon.objects.all()  # QuerySet 타입
+#     return render(request, 'pokemon/pokemon_list_html.html', {
+#         'pokemon_list': qs,
+#     })
 
 
 def pokemon_new(request):
